c5df98291bdfd7fff7009485e3e4c99d5b1d3318
Yulia Ivanenko <yulia.ivanenko@altarix.ru>
2018-10-25 16:30:52 +0400

    item menu shows or doesn't show splash screen

--- app/src/main/java/com/java/note/notekotlin/MainActivity.kt
+++ app/src/main/java/com/java/note/notekotlin/MainActivity.kt
@@ -3,28 +3,55 @@ package com.java.note.notekotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.FragmentManager
+import android.view.Menu
+import android.view.MenuItem
 import com.java.note.notekotlin.fragment.SplashFragment
+import com.java.note.notekotlin.utils.AppPreferences
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var fragmentManager: FragmentManager
+    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        AppPreferences.init(this)
+
         fragmentManager = supportFragmentManager
         runSplash()
     }
 
-    fun runSplash() {
-        val splashFragment = SplashFragment()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val splashItem = menu?.findItem(R.id.menuActionSplash)
+        splashItem?.isChecked = AppPreferences.firstRun
 
-        fragmentManager
-            .beginTransaction()
-            .replace(R.id.container, splashFragment)
-            .addToBackStack(null)
-            .commit()
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        if (id == R.id.menuActionSplash) {
+            item.isChecked = !item.isChecked
+            AppPreferences.firstRun = item.isChecked
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun runSplash() {
+        //show or don't show splash screen
+        if (!AppPreferences.firstRun) {
+
+            val splashFragment = SplashFragment()
+            fragmentManager
+                .beginTransaction()
+                .replace(R.id.container, splashFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/java/note/notekotlin/utils/AppPreferences.kt
@@ -0,0 +1,30 @@
+package com.java.note.notekotlin.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object AppPreferences {
+
+    private const val SPLASH_IS_INVISIBLE = "splash_is_invisible"
+
+    private const val NAME = "prefKotlin"
+    private const val MODE = Context.MODE_PRIVATE
+    private lateinit var preferences: SharedPreferences
+
+    fun init(context: Context) {
+        preferences = context.getSharedPreferences(NAME, MODE)
+    }
+
+    private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
+        val editor = edit()
+        operation(editor)
+        editor.apply()
+
+    }
+
+    var firstRun: Boolean
+        get() = preferences.getBoolean(SPLASH_IS_INVISIBLE, false)
+        set(value) = preferences.edit {
+            it.putBoolean(SPLASH_IS_INVISIBLE, value)
+        }
+}
