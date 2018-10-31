68295a4345a1c18d696a2fa892ea8016de8756d9
Yulia Ivanenko <yulia.ivanenko@altarix.ru>
2018-10-31 15:28:34 +0400

    delete onBind(), add circleView

--- app/src/main/java/com/java/note/notekotlin/model/ModelTask.kt
+++ app/src/main/java/com/java/note/notekotlin/model/ModelTask.kt
@@ -1,12 +1,44 @@
 package com.java.note.notekotlin.model
 
 import android.os.Parcelable
+import com.java.note.notekotlin.R
+import com.java.note.notekotlin.utils.Priority
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class ModelTask(var title: String, var date: Long) : Item, Parcelable {
+data class ModelTask(var title: String, var date: Long, var priority: Int, var status: Int) : Item, Parcelable {
 
-    constructor() : this("", 0)
+    constructor() : this("", 0L, 0, -1)
 
     override fun isTask() = true
+
+    fun getPriorityColor() =
+        when (priority) {
+            Priority.PRIORITY_HIGH -> {
+                if (isStatusCurrentOrOverdue()) {
+                    R.color.priority_high
+                } else {
+                    R.color.priority_high_selected
+                }
+            }
+
+            Priority.PRIORITY_NORMAL -> {
+                if (isStatusCurrentOrOverdue()) {
+                    R.color.priority_normal
+                } else {
+                    R.color.priority_normal_selected
+                }
+            }
+            Priority.PRIORITY_LOW -> {
+                if (isStatusCurrentOrOverdue()) {
+                    R.color.priority_low
+                } else {
+                    R.color.priority_low_selected
+                }
+            }
+            else -> 0
+        }
+
+    private fun isStatusCurrentOrOverdue() = status == Priority.STATUS_CURRENT ||
+            status == Priority.STATUS_OVERDUE
 }
